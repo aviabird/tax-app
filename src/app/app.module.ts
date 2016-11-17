7bcfd787d8389c2a7678880517ea113dcbfc46a3
Ashish Singh <aitashish173@gmail.com>
2016-11-17 17:22:12 +0530

    Added basic layout and setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { InvestorDetailComponent } from './components/investor-detail/investor-detail.component';
 import { TaxDetailComponent } from './components/tax-detail/tax-detail.component';
 import { ElssDetailComponent } from './components/elss-detail/elss-detail.component';
 import { PlatformsComponent } from './components/platforms/platforms.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+
+import { routes } from './routes';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +21,16 @@ import { PlatformsComponent } from './components/platforms/platforms.component';
     InvestorDetailComponent,
     TaxDetailComponent,
     ElssDetailComponent,
-    PlatformsComponent
+    PlatformsComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
